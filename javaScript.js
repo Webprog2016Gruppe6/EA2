a3588248dab9c2d014ecd9b2bd0a6d8f329b2431
rpnmhf <rpnmhf@users.noreply.github.com>
2016-11-20 17:46:10 +0100

    javaScript Semesterprojekt Anfang

---
+++ javaScript.js
@@ -0,0 +1,14 @@
+/**
+ * Gruppe 6 Semesterprojekt
+ */
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#stundenplaner').DataTable( {
+        "scrollY": 200,
+        "scrollX": true,
+        "filter": false,
+        "info": false,
+        "paging": false
+    } );
+} );
+</script>
